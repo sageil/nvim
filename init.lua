359a47e1c9a9924a8883034df24e8884325a071d
Sammy Ageil <sammy.ageil@outlook.com>
2025-07-21 22:43:35 -0400

    refactor: add transparency in init.lua

--- init.lua
+++ init.lua
@@ -1,10 +1,33 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 
 require("config.lazy")
--- function Transparent(color)
---   color = color or "tokyonight"
---   vim.cmd.colorscheme(color)
---   vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
---   vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
--- end
--- Transparent()
+
+local function set_transparency()
+  vim.cmd([[
+hi Normal guibg=NONE ctermbg=NONE
+hi NormalNC guibg=NONE ctermbg=NONE
+hi SignColumn guibg=NONE ctermbg=NONE
+hi StatusLine guibg=NONE ctermbg=NONE
+hi StatusLineNC guibg=NONE ctermbg=NONE
+hi VertSplit guibg=NONE ctermbg=NONE
+hi TabLine guibg=NONE ctermbg=NONE
+hi TabLineFill guibg=NONE ctermbg=NONE
+hi TabLineSel guibg=NONE ctermbg=NONE
+hi Pmenu guibg=NONE ctermbg=NONE
+hi PmenuSel guibg=NONE ctermbg=NONE
+hi NeoTreeNormal guibg=NONE ctermbg=NONE
+hi NeoTreeNormalNC guibg=NONE ctermbg=NONE
+hi NeoTreeWinSeparator guibg=NONE ctermbg=NONE
+hi NeoTreeEndOfBuffer guibg=NONE ctermbg=NONE
+hi EndOfBuffer guibg=NONE ctermbg=NONE
+]])
+end
+
+-- Apply transparency settings initially
+set_transparency()
+
+-- Reapply transparency on buffer enter
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = "*",
+  callback = set_transparency,
+})
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,7 +1,32 @@
+if true then
+  return {}
+end
+-- return {
+--   "rose-pine/neovim",
+--   name = "rose-pine-main",
+--   config = function()
+--     vim.cmd("colorscheme rose-pine-main")
+--   end,
+-- }
 return {
-  "rose-pine/neovim",
-  name = "rose-pine-moon",
-  config = function()
-    vim.cmd("colorscheme rose-pine-moon")
-  end,
+  -- add gruvbox
+  { "rose-pine/neovim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = function()
+        vim.schedule(function()
+          if vim.o.background == "light" then
+            vim.cmd("colorscheme rose-pine-dawn")
+          else
+            vim.cmd("colorscheme rose-pine-main")
+          end
+        end)
+      end,
+
+      --"rose-pine-main",
+    },
+  },
 }
